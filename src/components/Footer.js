b5de4b57a07d8cd8446b4436f2e55fdb42d54756
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2024-05-31 06:48:56 -0400

    added all projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
+// import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon1 from "../assets/img/linkedin.svg";
+
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import gitIcon from "../assets/img/gitIcon.svg"
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col size={12} className="text-center">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/mohanpreetsn/"><img src={navIcon1} alt="Icon" /></a>
+              <a id="small-link" href="https://github.com/MohanpreetS"><img id="small-icon"  src={gitIcon} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 // import logo from '../assets/img/logo.svg';
 import logo from '../assets/img/pfp.jpeg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
+// import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon1 from "../assets/img/linkedin.svg";
+
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import gitIcon from "../assets/img/gitIcon.svg"
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -51,9 +54,8 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/mohanpreetsn/"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/mohanpreetsn/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/MohanpreetS" target="_blank"><img id="small-icon" src={gitIcon} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd" style={{"border-radius": "20px"}}><span>Let’s Connect</span></button>
